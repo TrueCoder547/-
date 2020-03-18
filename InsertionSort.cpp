489544f64e7aecc9a170d35ff584929508e7e643
TrueCoder547 <61290878+TrueCoder547@users.noreply.github.com>
2020-03-18 09:57:48 +0300

    Add files via upload

---
+++ InsertionSort.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int* InsertionSort(int array[], int size);
+int main() {
+	setlocale(NULL, "");
+	const int size = 5;
+	int array[size] = { 2,5,7,4,12 };
+	int* ptrarray = new int[size];
+	ptrarray = InsertionSort(array, size);
+	return 0;
+}
+int* InsertionSort(int array[], int size) {
+	for ( int i = 1; i < size; i++) {
+		for (int j = i; j > 0 && array[j - 1] > array[j]; j--)  
+			swap(array[j - 1], array[j]);
+		for (int i = 0; i < size; i++) {
+			cout << array[i] << "\t";
+		}
+		return array;
+	}
+} 
